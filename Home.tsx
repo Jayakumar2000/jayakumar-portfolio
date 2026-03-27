fd5078c75770e01150dd4c03816e0d9270e66bc7
Jayakumar2000 <67823193+Jayakumar2000@users.noreply.github.com>
2026-03-27 12:53:30 +0530

    Update Home.tsx

--- Home.tsx
+++ Home.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Hero } from "../components/Hero";
-import { ProjectGrid } from "../components/ProjectGrid";
-import { StoryHook } from "../components/StoryHook";
-import { Toolkit } from "../components/Toolkit";
-import { CTA } from "../components/CTA";
+import { Hero } from "./Hero";
+import { ProjectGrid } from "./ProjectGrid";
+import { StoryHook } from "./StoryHook";
+import { Toolkit } from "./Toolkit";
+import { CTA } from "./CTA";
 
 export function Home() {
   return (
